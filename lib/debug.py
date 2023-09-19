3e8934a0c24ea68284e2256168c022e705b0fb4d
Ricardo Guerra <RikkuG491@gmail.com>
2023-09-19 11:41:14 -0400

    Update debug.py

--- lib/debug.py
+++ lib/debug.py
@@ -8,4 +8,14 @@ from classes.many_to_many import Coffee
 if __name__ == '__main__':
     print("HELLO! :) let's debug")
 
+    customer1 = Customer("Alice")
+    customer2 = Customer("Bob")
+    customer3 = Customer("Chris")
+    coffee1 = Coffee("Cappuccino")
+    coffee2 = Coffee("Starbucks")
+    coffee3 = Coffee("Matcha Tea Latte")
+    order1 = Order(customer1, coffee1, 30)
+    order2 = Order(customer2, coffee2, 30)
+    order3 = Order(customer1, coffee1, 40)
+
     ipdb.set_trace()
